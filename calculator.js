b785bfac27c22c43b2430369aaaefa59927a44fb
Joshua Colvin <joshua@joshuacolvin.net>
2017-02-06 05:57:58 -0500

    Add test for one and zero

--- calculator.js
+++ calculator.js
@@ -109,4 +109,5 @@
   Test.assertEquals(four(plus(nine())), 13);
   Test.assertEquals(eight(minus(three())), 5);
   Test.assertEquals(six(dividedBy(two())), 3);
+  Test.assertEquals(one(plus(zero())), 1);
 })()
